9822f1a75d64ffc09f1daf9552830ca2933ed84c
AD <66907615+add830830@users.noreply.github.com>
2021-04-01 16:54:36 +0530

    Update thumbnail.py

--- lazyleech/plugins/thumbnail.py
+++ lazyleech/plugins/thumbnail.py
@@ -4,7 +4,7 @@ from pyrogram import Client, filters
 from .. import ALL_CHATS, help_dict
 from ..utils.misc import convert_to_jpg, get_file_mimetype, watermark_photo
 
-@Client.on_message(filters.command(['thumbnail', 'savethumbnail', 'setthumbnail']) & filters.chat(ALL_CHATS))
+@Client.on_message(filters.command(['setthumbnail@Anu1Bot', 'setthumbnail']) & filters.chat(ALL_CHATS))
 async def savethumbnail(client, message):
     reply = message.reply_to_message
     document = message.document
@@ -41,7 +41,7 @@ async def savethumbnail(client, message):
     else:
         await message.reply_text('Cannot find thumbnail')
 
-@Client.on_message(filters.command(['clearthumbnail', 'rmthumbnail', 'delthumbnail', 'removethumbnail', 'deletethumbnail']) & filters.chat(ALL_CHATS))
+@Client.on_message(filters.command(['delthumbnail@Anu1Bot', 'delthumbnail']) & filters.chat(ALL_CHATS))
 async def rmthumbnail(client, message):
     for path in ('thumbnail', 'watermarked_thumbnail'):
         path = os.path.join(str(message.from_user.id), f'{path}.jpg')
@@ -50,12 +50,5 @@ async def rmthumbnail(client, message):
     await message.reply_text('Thumbnail cleared')
 
 help_dict['thumbnail'] = ('Thumbnail',
-'''/thumbnail <i>&lt;as reply to image or as a caption&gt;</i>
-/setthumbnail <i>&lt;as reply to image or as a caption&gt;</i>
-/savethumbnail <i>&lt;as reply to image or as a caption&gt;</i>
-
-/clearthumbnail
-/rmthumbnail
-/removethumbnail
-/delthumbnail
-/deletethumbnail''')
+'''/setthumbnail <i>&lt;As Reply to Image or as a Caption&gt;</i>
+/delthumbnail <i>&lt;Just Send to Delete Thumbnail For Further Uploads.&gt;</i>''')
